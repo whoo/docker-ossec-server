aa82508cef414fd7257efe4904d6c17a2f61fbf0
Dominique DERRIER <derrierdo@gmail.com>
2016-06-03 07:43:10 -0400

    Add openssl

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+
+openssl genrsa -out /var/ossec/etc/sslmanager.key 4096
+openssl req -new  -x509 -key  /var/ossec/etc/sslmanager.key -out /var/ossec/etc/sslmanager.cert -days 365 -subj /CN=${HOSTNAME}/
+
 /var/ossec/bin/ossec-authd &
 /etc/init.d/ossec start
 

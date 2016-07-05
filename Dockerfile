9ed2b66bc8c528abdbbbb8c4562aa6ed396ee359
Dominique DERRIER <derrierdo@gmail.com>
2016-07-04 21:09:39 -0400

    Docker

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,7 @@ RUN apt-key adv --fetch-keys http://ossec.wazuh.com/repos/apt/conf/ossec-key.gpg
 RUN echo "deb http://ossec.wazuh.com/repos/apt/debian jessie main" >> /etc/apt/sources.list
 RUN apt-get -y update && apt-get -y upgrade && apt-get -y install ossec-hids openssl
 COPY run.sh /
+RUN cp -a /var/ossec/etc /var/ossec/etc.orig
+RUN cp -a /var/ossec/logs /var/ossec/logs.orig
+RUN touch /var/ossec/etc/process_list && ln /var/ossec/etc/process_list /var/ossec/bin/.process_list
 ENTRYPOINT /run.sh
--- run.sh
+++ run.sh
@@ -1,11 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 
 openssl genrsa -out /var/ossec/etc/sslmanager.key 4096
 openssl req -new  -x509 -key  /var/ossec/etc/sslmanager.key -out /var/ossec/etc/sslmanager.cert -days 365 -subj /CN=${HOSTNAME}/
 
 /var/ossec/bin/ossec-authd &
+PID=$!
+
+if [ -e /var/ossec/etc/process_list ] 
+then
+	ln -fs /var/ossec/etc/process_list /var/ossec/bin/.process_list
+fi
+
+
 /etc/init.d/ossec start
+
+function ossec_shutdown(){
+/var/ossec/bin/ossec-control stop;
+#kill $PID
+}
+
+trap "ossec_shutdown; exit" SIGINT SIGTERM
+
+
 
 while true
 do
